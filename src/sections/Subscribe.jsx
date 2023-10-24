35b5ba622f3669f801dff0f24721f6fbbbeadcc3
Anwar-Barakat <brkatanwar0@gmail.com>
2023-10-24 11:15:27 +0300

    finished react nike project

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -3,11 +3,10 @@ import { Button } from "../components/Button";
 const Subscribe = () => {
   return (
     <section className="max-container flex justify-between items-center max-lg:flex-col gap-10">
-      <h3 className="text-4xl leading-10 lg:max-w-md font-palanquin font-bold">
-        <span>Sign up form </span>
+      <h3 className="text-4xl leading-[72px] lg:max-w-lg font-palanquin font-bold">
+        <span className="block">Sign up form </span>
         <span className="text-coral-red"> Updates </span>
-        <span> & </span>
-        <span> Newsletter </span>
+        <span> & Newsletter </span>
       </h3>
       <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
         <input type="text" className="input" placeholder="Subscribe Now" />
